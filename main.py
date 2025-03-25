7e092919d2e603a6baef27ff674d016143e24f6f
Nils Eckmann <nils.eckmann@mein.gmx>
2025-03-25 06:59:21 +0100

    in progress

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 # 84 Tic Tac Toe
 
-def print_field():
-    print(field[0:3])
-    print(field[3:6])
-    print(field[6:9])
+def print_field(pf_field):
+    print(pf_field[0:3])
+    print(pf_field[3:6])
+    print(pf_field[6:9])
 
 
-def choose_player(cp_round, cp_players):
+def choose_player(cp_round: int, cp_players: list) -> str:
+    """
+    Returns the current player depending on played rounds.
+    :param cp_round:
+    :param cp_players:
+    :return:
+    """
     if cp_round % 2 == 1:
         return cp_players[0]
     else:
         return cp_players[1]
 
 
-def free_field():
+def check_field() -> bool:
+
     if "-" in field:
         return True
     else:
@@ -30,16 +37,16 @@ if __name__ == "__main__":
              "-", "-", "-",
              "-", "-", "-"]
 
-    player = choose_player(game_round, players)
+    print_field(field)
+    is_free = check_field()
 
-    print(f"Player 1: {players[0]}, Player 2: {players[1]},")
-    print_field()
-    print(free_field())
 
     while is_running:
         player = choose_player(game_round, players)
         print(f"Current player {player}")
-
+        player_field = int(input(f"Choose a field 0-8: "))
+        field[player_field] = player
+        print_field(field)
         next_round = input("Play another round y/n: ")
         game_round += 1
         if next_round == "n":
